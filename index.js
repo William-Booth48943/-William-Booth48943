45fcba636df33af8e0ea767fd85b79e52406b437
William Booth <boothy.w@gmail.com>
2023-08-04 19:33:45 +0100

    Add email validation

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-async function postFormDataAsJson({ url, formData }) {
-	const plainFormData = Object.fromEntries(formData.entries());
-	const formDataJsonString = JSON.stringify(plainFormData);
+async function postEmail({ url, email }) {
+	const formDataJsonString = JSON.stringify({email});
 
 	const fetchOptions = {
 		method: "PUT",
-		// headers: {
-		// 	//"Content-Type": "application/json",
-		// 	//Accept: "application/json",
-		// },
 		body: formDataJsonString,
 	};
 
@@ -17,25 +12,38 @@ async function postFormDataAsJson({ url, formData }) {
 		const errorMessage = await response.text();
 		throw new Error(errorMessage);
 	}
-
+    let button = document.getElementById("sign-up");
+    button.innerHTML = "Submitted";
+    button.disabled = true;
+    button.classList.add("submitted");
 	return response.json();
 }
 
 async function handleFormSubmit(event) {
 	event.preventDefault();
-
-	const form = event.currentTarget;
 	const url = 'https://nkwi8qu7fl.execute-api.eu-west-2.amazonaws.com/Interest';
-
 	try {
-		const formData = new FormData(form);
-		const responseData = await postFormDataAsJson({ url, formData });
-
-		console.log({ responseData });
+        let email = document.getElementById("email").value;
+        let response = document.getElementById("response");
+        if(!validateEmail(email)){
+            response.innerHTML = "Please enter a valid email";
+            return;
+        }
+        else {
+            response.innerHTML = "";
+        }
+		const responseData = await postEmail({ url, email });
+	//	console.log({ responseData });
 	} catch (error) {
-		console.error(error);
+	//	console.error(error);
 	}
 }
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
 
 window.onload = function()
 {
